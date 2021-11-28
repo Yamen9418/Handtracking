48f81a4119d3ea9a9b91b1ac885de5d28ca0db19
mo4718al-s <mo4718al-s@student.lu.se>
2021-11-28 20:36:18 +0100

    tracking points work so far

--- HandTrackingMin.py
+++ HandTrackingMin.py
@@ -6,17 +6,19 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
-
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) #convert img to RGB
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # convert img to RGB
     results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks) # to check that we get value of tracking hand
 
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)  # cause we are displaying img not imgRGB
 
 
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
